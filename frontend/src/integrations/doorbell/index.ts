b587574e2e98801b9a34f7aff43a4a03f61ead1b
bbaldino <bbaldino@gmail.com>
2026-07-22 14:30:17 -0700

    feat(doorbell): add ring listener with edge detection, chime, auto-dismiss

--- frontend/src/integrations/doorbell/index.ts
+++ frontend/src/integrations/doorbell/index.ts
@@ -1,3 +1,4 @@
 export { DoorbellWidget } from './DoorbellWidget'
 export { useWebRtcStream } from './useWebRtcStream'
 export { doorbellIntegration } from './config'
+export { DoorbellRingListener } from './DoorbellRingListener'
